0e6181657e8b7b1f42222ca6ad7e2e3528884573
YashSingh15 <yashpsingh.edu@gmail.com>
2023-05-06 14:14:23 +0530

    Add basic calculator layout

--- index.js
+++ index.js
@@ -13,3 +13,20 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(operand1, operator, operand2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case 
+            ''
+    }
+}
+
+let operand1;
+let operator;
+let operand2;
