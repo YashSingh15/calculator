e8b4c0935203721f427865756c77e5f74bfdc70e
YashSingh15 <yashpsingh.edu@gmail.com>
2023-05-07 00:06:52 +0530

    Update calculator logic and UI, refactor operator handling
    This commit updates the calculator logic and user interface by:
    - Changing operator symbols to +, −, ×, ÷ and adding new constants for
    operators and other special buttons
    - Refactoring the operate function to use the new operator constants
    - Introducing the `isOperator` function to check if a given text is an
    operator
    - Updating the `updateDisplay` function to handle operator selection,
    new number entry, and equal button presses
    - Streamlining the display update flow and resetting state for new
    calculations

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const display = document.querySelector('.display');
+
+const ADD = '+';
+const SUBTRACT = '−';
+const MULTIPLY = '×';
+const DIVIDE = '÷';
+const EQUALS = '=';
+const PLUS_MINUS = '±';
+
+let result = 0;
+let operator = '';
+let newEntry = false;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -14,41 +27,71 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate(operand1, operator, operand2) {
+function operate(num1, operator, num2) {
     switch (operator) {
-        case 'add':
+        case ADD:
             return add(num1, num2);
-        case 'subtract':
+        case SUBTRACT:
             return subtract(num1, num2);
-        case 'multiply':
+        case MULTIPLY:
             return multiply(num1, num2);
-        case 'divide':
+        case DIVIDE:
             return divide(num1, num2)
     }
 }
 
 function clearDisplay() {
-    const display = document.querySelector('.display');
     display.textContent = '0';
 }
 
+function isOperator(text) {
+    const operators = [ADD, SUBTRACT, MULTIPLY, DIVIDE];
+    return operators.includes(text);
+}
+
+
 function updateDisplay(e) {
-    const display = document.querySelector('.display');
     const selectedButtonText = e.target.textContent;
 
     if (selectedButtonText === 'AC') {
+        result = 0;
+        operator = '';
         clearDisplay();
         return;
     }
 
-    // two paths
-    // if display shows single 0, then replace that
-    // if display shows any other number or combination of numbers, add to that list
+    if (isOperator(selectedButtonText)) {
+        result = +display.textContent;
+        operator = selectedButtonText;
+        newEntry = true;
+        return;
+    }
+
+    if (selectedButtonText === EQUALS) {
+        if (operator === '') {
+            return;
+        }
+
+        const num1 = result;
+        const num2 = +display.textContent;
+
+        result = operate(num1, operator, num2);
+        display.textContent = result;
+
+        operator = '';
+        return;
+    }
 
     if (display.textContent === '0') {
         display.textContent = selectedButtonText;
     } else {
-        display.textContent += selectedButtonText;
+        if (newEntry) {
+            clearDisplay();
+            display.textContent = selectedButtonText;
+            newEntry = false;
+        } else {
+            display.textContent += selectedButtonText;
+        }
     }
 }
 
